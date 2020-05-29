b0de6104231332c02033ae1db01e441fb467140a
Sunil Sahu <sahu03990@gmail.com>
2020-05-29 10:29:03 +0530

    dropdown added

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -15,11 +15,24 @@ export class ListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log('In alist.component.ts onInit() function ')
   }
 
   getCurrentUser(user) : void {
       this.selectedUser=user;
       console.log(this.selectedUser);
       console.log(`selectedUser = ${JSON.stringify(this.selectedUser)}`);
+  }
+  getAll(){
+    console.log("All selected");
+  }
+  getPending(){
+    console.log("Pending selected");
+  }
+  getApproved(){
+    console.log("Approved selected");
+  }
+  getRejected(){
+    console.log("Rejected selected");
   }
 }
